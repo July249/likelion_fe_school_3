5e041580b4ef4fa2d3b42adb1c9599028c1ec48c
jihyunglee <jhlee7125@gmail.com>
2022-10-23 09:54:54 +0900

    NodeJS Tutorial - Middleware 22/10/23

--- online_course/node-js/js_test/first-project/app.js
+++ online_course/node-js/js_test/first-project/app.js
@@ -19,6 +19,12 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// 어떠한 요청에도 무조건 중간에 한번은 동작이 되도록 작성
+// app.use((req, res, next) => {
+//   console.log('middle ware');
+//   next();
+// });
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
--- online_course/node-js/js_test/first-project/routes/index.js
+++ online_course/node-js/js_test/first-project/routes/index.js
@@ -1,46 +1,66 @@
 var express = require('express');
 var router = express.Router();
+const loginCheck = require('../module/loginCheck');
 
-let arr = [];
+/* loginCheck - Middleware */
+// const loginCheck = (req, res, next) => {
+//   const userLogin = false;
+//   if (userLogin) {
+//     next();
+//   } else {
+//     res.status(400).json({
+//       message: 'login fail!!',
+//     });
+//   }
+// };
+
+router.get('/', loginCheck, (req, res) => {
+  res.status(200).json({
+    message: 'login success!',
+  });
+});
+
+/* HTTP Method */
+// let arr = [];
 
 // GET method
-router.get('/read', (req, res) => {
-  res.status(200).json({
-    message: 'read success',
-  });
-});
+// router.get('/read', (req, res) => {
+//   res.status(200).json({
+//     message: 'read success',
+//   });
+// });
 
 // POST method
-router.post('/create', (req, res) => {
-  const { data } = req.body;
-  arr.push(data);
-  res.status(200).json({
-    message: 'create success',
-    result: arr,
-  });
-  console.log(req.body);
-});
+// router.post('/create', (req, res) => {
+//   const { data } = req.body;
+//   arr.push(data);
+//   res.status(200).json({
+//     message: 'create success',
+//     result: arr,
+//   });
+//   console.log(req.body);
+// });
 
 // PUT method
-router.put('/update/:id', (req, res) => {
-  const { id } = req.params;
-  const { data } = req.body;
-  arr[id] = data;
-  res.status(200).json({
-    message: 'update success',
-    result: arr,
-  });
-  console.log(id);
-});
+// router.put('/update/:id', (req, res) => {
+//   const { id } = req.params;
+//   const { data } = req.body;
+//   arr[id] = data;
+//   res.status(200).json({
+//     message: 'update success',
+//     result: arr,
+//   });
+//   console.log(id);
+// });
 
 // DELETE method
-router.delete('/delete/:id', (req, res) => {
-  const { id } = req.params;
-  arr.splice(id, 1);
-  res.status(200).json({
-    message: 'delete success',
-    result: arr,
-  });
-});
+// router.delete('/delete/:id', (req, res) => {
+//   const { id } = req.params;
+//   arr.splice(id, 1);
+//   res.status(200).json({
+//     message: 'delete success',
+//     result: arr,
+//   });
+// });
 
 module.exports = router;
