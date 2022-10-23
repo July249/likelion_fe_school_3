a6a0e9f21cd915cec758261ada30a85536490a64
jihyunglee <jhlee7125@gmail.com>
2022-10-24 00:03:40 +0900

    NodeJS Tutorial - Pet SNS 게시물 수정 및 삭제 22/10/23

--- online_course/node-js/pet-sns/controller/postController.js
+++ online_course/node-js/pet-sns/controller/postController.js
@@ -42,6 +42,70 @@ const postController = {
     const post = await Post.findById(id);
     res.render('detail', { post: post });
   },
+  updateLayout: async (req, res) => {
+    const { id } = req.params;
+    const post = await Post.findById(id);
+    res.render('update', { post: post });
+  },
+  update: async (req, res) => {
+    const { id } = req.params;
+    const { title, content } = req.body;
+    try {
+      await Post.findByIdAndUpdate(
+        id,
+        { title: title, content: content },
+        { new: true }
+      );
+      res.redirect('/');
+    } catch (error) {
+      res.status(500).send('update error!!');
+    }
+  },
+  delete: async (req, res) => {
+    const { id } = req.params;
+    try {
+      await Post.findByIdAndDelete(id);
+      res.redirect('/');
+    } catch (error) {
+      res.status(500).send('delete error!!');
+    }
+  },
+  // like: async (req, res) => {
+  //   const { id } = req.params;
+  //   const post = await Post.findById(id);
+  //   const check = post.likeUser.some((userId) => {
+  //     return userId === req.userInfo._id;
+  //   });
+  //   if (check) {
+  //     post.likeCount -= 1;
+  //     const idx = post.likeUser.indexOf(req.userInfo._id);
+  //     if (idx > -1) {
+  //       post.likeUser.splice(idx, 1);
+  //     }
+  //   } else {
+  //     post.likeCount += 1;
+  //     post.likeUser.push(req.userInfo._id);
+  //   }
+  //   const result = await post.save();
+  //   res.status(200).json({
+  //     check: check,
+  //     post: result,
+  //   });
+  // },
+  // comment: async (req, res) => {
+  //   const { id } = req.params;
+  //   const post = await Post.findById(id);
+  //   const user = req.userInfo;
+  //   const { comment } = req.body;
+  //   const commnetWrap = {
+  //     comment: comment,
+  //     user: user,
+  //   };
+
+  //   post.comment.push(commnetWrap);
+  //   const result = await post.save();
+  //   res.status(200).json({ post: result });
+  // },
 };
 
 module.exports = postController;
--- online_course/node-js/pet-sns/routes/posts/index.js
+++ online_course/node-js/pet-sns/routes/posts/index.js
@@ -10,16 +10,16 @@ router.get('/upload', (req, res) => {
 
 router.get('/:id', postController.detail);
 
-router.get('/update/:id', (req, res) => {
-  res.render('update');
-});
+router.get('/update/:id', postController.updateLayout);
 // router.get("/update/:id", checkUser, postController.updateLayout);
 
 router.post('/', upload.single('image'), postController.upload);
 // router.post("/", checkUser, upload.single("image"), postController.upload);
 
+router.post('/update/:id', postController.update);
 // router.post("/update/:id", checkUser, postController.update);
 
+router.post('/delete/:id', postController.delete);
 // router.post("/delete/:id", checkUser, postController.delete);
 
 // router.post("/like/:id", checkUser, postController.like);
