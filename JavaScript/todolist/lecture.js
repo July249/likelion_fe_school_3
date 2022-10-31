0f6980b40659e07cb46a4485779d56e65d9a9a3b
jihyunglee <jhlee7125@gmail.com>
2022-10-31 09:03:36 +0900

    2022-10-27

--- JavaScript/todolist/lecture.js
+++ JavaScript/todolist/lecture.js
@@ -9,92 +9,6 @@ const listTodo = main.querySelector('.list_todo');
 
 // 할일 추가 이벤트
 addBtn.addEventListener('click', createListItem);
-// ========================
-// 경고 메세지 생성
-const message = document.createElement('strong');
-message.style.display = 'none';
-message.classList.add('txt_invalid');
-main.appendChild(message);
-userTask.addEventListener('input', () => {
-  message.style.display = 'none';
-});
-
-// 다운로드 버튼 생성
-const downloadBtn = document.createElement('button');
-downloadBtn.classList.add('btn');
-downloadBtn.textContent = '리스트 다운로드';
-main.appendChild(downloadBtn);
-downloadBtn.addEventListener('click', downloadFile);
-
-// 투두를 저장할 배열
-// 'tasklist'라는 키를 가진 localStroage에 저장된 값(문자열)을
-// 객체로 불러온다.
-// Note: JSON.stringify() 객체 => 문자열
-//       JSON.parse()     문자열 => 객체
-// 단축 평가를 이용함 (단, B는 항상 true === 값이 있음)
-// - A && B => A가 false면 A && B를 false로 판단 => A를 반환
-// - A || B => A가 false면 A || B를 true로 판단 => B를 반환
-// 정리!
-/* 
-A        B           평가결과
-true  || anything => true
-false || anything => anything
-true  && anything => anything
-false && anything => false
-*/
-/* const a = undefined;
-const b = 'hello world';
-if (a && b) {
-  arr.push(c);
-} */
-const tasks = JSON.parse(localStorage.getItem('tasklist')) || [];
-
-// 초기 화면에서 저장되어있는 데이터를 가지고 목록을 생성합니다.
-if (tasks.length > 0) {
-  tasks.forEach((task) => {
-    genItem(task.val, task.checked);
-  });
-  showDownload();
-}
-
-function createListItem() {
-  // input에 입력된 값을 inpVal 변수에 저장한다.
-  const inpVal = userTask.value;
-
-  if (inpVal /* inpVal에 값이 있으면 */) {
-    // 할일을 저장하는 객체를 생성합니다.
-    const tempTask = {
-      val: inpVal,
-      checked: false, // default setting
-    };
-
-    // 할일 목록에 새로운 할일을 저장합니다.
-    tasks.push(tempTask);
-
-    // 목록 요소를 생성합니다.
-    genItem(inpVal, false);
-
-    // 할일 데이터를 localStorage 에 저장합니다.
-    saveTasks();
-
-    // 다운로드 버튼 노출 함수
-    showDownload();
-  } else {
-    errorMsg('내용을 작성해주세요');
-  }
-}
-
-// 최상위 부모 요소
-const main = document.querySelector('.artcl_main');
-// input요소
-const userTask = main.querySelector('header input');
-// input의 Button
-const addBtn = main.querySelector('header button');
-// ul요소
-const listTodo = main.querySelector('.list_todo');
-
-// 할일 추가 이벤트
-addBtn.addEventListener('click', createListItem);
 
 // 경고 메세지 생성
 const message = document.createElement('strong');
@@ -172,7 +86,7 @@ function createListItem() {
 }
 
 // 목록 요소를 생성합니다.
-function genItem(val, complete/*파라미터*/) {
+function genItem(val, complete /*파라미터*/) {
   //li 요소 생성
   const li = document.createElement('li');
   li.textContent = val;
@@ -214,7 +128,10 @@ function genItem(val, complete/*파라미터*/) {
 
 // localStorage 에 할일 목록을 저장하는 함수
 function saveTasks() {
-  localStorage.setItem('tasklist', JSON.stringify(tasks)/* task에 저장된 값들을 문자열로 변환 */);
+  localStorage.setItem(
+    'tasklist',
+    JSON.stringify(tasks) /* task에 저장된 값들을 문자열로 변환 */
+  );
   /* 변환된 문자열을 tasklist 키의 값으로 하여 localStroage에 저장 */
 }
 
@@ -226,7 +143,7 @@ function buildTasks() {
   const curList = listTodo.querySelectorAll('li');
 
   // 할일 정보 목록을 재생성합니다.
-  // curList.filter((el) => !el.checked); 
+  // curList.filter((el) => !el.checked);
   // 이미 수행한 정보는 모두 사라지고, 아직 수행하지 못한 정보만 남아있게 됩니다.
   // filter 사용시 문제점 (요구사항에 따라서 문제가 될수도 있음)
   // - 삭제 버튼을 사용자가 누르지 않았는데, 단순히 done 클래스명이 들어갔다는 이유만으로 알아서 삭제가 되는 문제가 발생
@@ -273,7 +190,7 @@ function downloadFile() {
   curList.forEach((el) => {
     if (el.classList.contains('done')) {
       temp += '완료 - ';
-      // '완료 -'를 붙일지 말지 판단 
+      // '완료 -'를 붙일지 말지 판단
     }
     temp += `${el.textContent}\n`;
     // li의 내용
